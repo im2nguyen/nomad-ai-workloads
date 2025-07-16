7703ae2673649fe264ef675ed6869cc035c2e5fe
Anthony <russo555@gmail.com>
2025-07-16 15:48:47 -0400

    Modified bucket name to include random suffix and added force_destroy.

--- nomad/aws-bucket.tf
+++ nomad/aws-bucket.tf
@@ -1,6 +1,7 @@
 # Bucket for Open WebUI backend
 resource "aws_s3_bucket" "openwebui_bucket" {
-  bucket = "openwebui-backend-bucket"
+  bucket = "openwebui-backend-bucket-${local.prefix}"
+  force_destroy = true
 
   tags = {
     Name        = "Open WebUI bucket"

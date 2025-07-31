ac5ad60d8b14c5e2e17c3930b9ce3cd0707b2acc
Anthony <russo555@gmail.com>
2025-07-31 12:11:29 -0400

    Cleanup

--- nomad/aws-outputs.tf
+++ nomad/aws-outputs.tf
@@ -7,15 +7,3 @@ output "nomad_management_token" {
   value = random_uuid.nomad_mgmt_token.result
   sensitive = false
 }
-
-# output "nomad_user_token" {
-#   value = nonsensitive(nomad_acl_token.nomad_user_token.secret_id)
-# }
-
-# output "debug_aws_server_ssh" {
-#   value = "ssh -i certs/aws-key-pair.pem ubuntu@${aws_instance.server[0].public_ip}"
-# }
-
-# output "debug_aws_client_ssh" {
-#   value = "ssh -i certs/aws-key-pair.pem ubuntu@${aws_instance.client[0].public_ip}"
-# }
